300a5e4984cd12d6220e98d79390da976f8891ff
dron5 <dron5@mail.ru>
2023-06-08 12:24:40 +0700

    + NotFoundPage LoadingPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,10 +4,11 @@ import { Routes, Route } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader/>}>
       <Routes>
         {Object.values(routeConfig).map(({ element, path }) => (
           <Route key={path} path={path} element={element} />
