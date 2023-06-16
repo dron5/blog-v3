77533fb33f15a6014974921a4c4146fe09faa0f7
dron5 <dron5@mail.ru>
2023-06-16 13:26:11 +0700

    + storyBook

---
+++ config/storybook/main.js
@@ -0,0 +1,31 @@
+module.exports = {
+  stories: [
+    '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
+
+// эта херня генерится автоматом и не работает
+// module.exports = {
+//   stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
+//   addons: [
+//     '@storybook/addon-links',
+//     '@storybook/addon-essentials',
+//     '@storybook/addon-interactions',
+//   ],
+//   framework: {
+//     name: '@storybook/react-webpack5',
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: 'tag',
+//   },
+// };
